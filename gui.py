c971a2e60fc033b03cb0174221777fb2730bed66
0xC4 <c.roest@student.rug.nl>
2019-06-22 14:05:00 +0200

    Text color stuff moved to gui.py

--- gui.py
+++ gui.py
@@ -1,3 +1,17 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+ANMT = bcolors.FAIL + "[A]" + bcolors.ENDC
+INFO = bcolors.OKBLUE + "[I]" + bcolors.ENDC
+VALD = bcolors.WARNING + "[V]" + bcolors.ENDC
+
 class GUI:
     def __init__(self, ks):
         self.knowledgestructure = ks
