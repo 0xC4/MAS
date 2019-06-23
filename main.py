f5c75e8a063bc9298cce5ece2aacc0ca3e096f64
Quintin1995 <Quintin1995>
2019-06-23 16:40:27 +0200

    made error bar plot, this results in many more games played in total

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ def get_policy_label(turn_agent, knowledgestructure, game_idx, n):
 
 #plot a barplot that shows win results for each player
 #by default the plot is NOT saved to a file.
-def plot_bar_plot(win_results, plot_filename, do_save_figure=False):
+def plot_bar_plot(win_results, plot_filename, do_save_figure=False, do_show_figure=False):
     counts = dict()
     for i in win_results:
         counts[i] = counts.get(i, 0) + 1
@@ -169,10 +169,50 @@ def plot_bar_plot(win_results, plot_filename, do_save_figure=False):
     ax.invert_yaxis()  # labels read top-to-bottom
     ax.set_xlabel("Win Count")
     ax.set_title("Win counts for each Agent")
+
     if do_save_figure:
         plt.savefig(plot_filename)
         print("\nWrote (file) plot to: " + plot_filename)
-    plt.show()
+    if do_show_figure:
+        plt.show()
+
+
+#plots an error bar plot over the given win counts per policy
+def plot_error_bar(agent_a_win_count_list, agent_b_win_count_list, agent_c_win_count_list, win_counts, do_save_figure, do_show_figure, plot_filename):
+     # Calculate the average
+    agent_a_mean = np.mean(agent_a_win_count_list)
+    agent_b_mean = np.mean(agent_b_win_count_list)
+    agent_c_mean = np.mean(agent_c_win_count_list)
+
+    # Calculate the std
+    agent_a_std = np.std(agent_a_win_count_list)
+    agent_b_std = np.std(agent_b_win_count_list)
+    agent_c_std = np.std(agent_c_win_count_list)
+
+    labels      = sorted(list(win_counts.keys()))
+    x_pos       = np.arange(len(labels))
+    CTEs        = [agent_a_mean, agent_b_mean, agent_c_mean]
+    errors      = [agent_a_std, agent_b_std, agent_c_std]
+
+    fig, ax = plt.subplots()
+    ax.bar(x_pos, CTEs,
+        yerr=errors,
+        align='center',
+        alpha=0.5,
+        ecolor='black',
+        capsize=10)
+    ax.set_ylabel('Win Count')
+    ax.set_xticks(x_pos)
+    ax.set_xticklabels(labels)
+    ax.set_title('Mean win count')
+    ax.yaxis.grid(True)
+
+    plt.tight_layout()
+    if do_save_figure:
+        plt.savefig(plot_filename)
+        print("\nWrote (file) plot to: " + plot_filename)
+    if do_show_figure:
+        plt.show()
 
 #creates a plot filename based on program settings/parameters
 def create_plot_filename(policy_set, number_of_games_played):
@@ -192,47 +232,82 @@ def create_plot_filename(policy_set, number_of_games_played):
     plot_path += ".png"
     return plot_path
 
-############################################################################
 
-def run_avg_experiment(amount=10):
-    #the amount of games that will be played
-    n = 20
+def run_avg_experiment(avg_amount, amount, policy_set, do_save_figure, do_show_figure, plot_filename, show_menu_each_step):
+    agent_a_win_count_list = []
+    agent_b_win_count_list = []
+    agent_c_win_count_list = []
+    for exp in range(avg_amount):
 
-    #Whether you want to save the output barplot to a 
-    # file or not. It might overwrite an existing
-    # plot if you run the exact same settings as a
-    # previous experiment. It will be located in the
-    # folder "plots/"
-    do_save_figure = False
+        #Run the game n times with the given policies
+        # and store the results.
+        win_results = run_game(amount, policy_set, show_menu_each_step)
 
-    #Choose a policy for each agent:
-        # RANDOM                  = 0     #Chooses a random possible move
-        # CHOOSE_OTHER_PLAYER     = 1     #Favors choosing an announcement about another players rather than himself
-        # CHOOSE_THEMSELVES 	  = 2     #Favors choosing an announcement about himself rather than about another player.
-        # ARGMIN                  = 3     #Chooses the announcement that results in the lowest amount of possible worlds remaining after the announcement is made.
-        # ARGMAX                  = 4     #Chooses the announcement that results in the highest amount of possible worlds remaining afther the announcement is made.
-    policy_set = [Policies.RANDOM, Policies.RANDOM, Policies.RANDOM]
-
-    #Create a plot filename based on program input 
-    # parameters (policies and amount of games played).
-    plot_filename = create_plot_filename(policy_set, n)
-
-    #Set to true if you want a menu after each step
-    # in the game. If set to False, you can run
-    # many games and see results of them in a
-    # barplot.
-    show_menu_each_step = False
-
-    #Run the game n times with the given policies
-    # and store the results.
-    win_results = run_game(n, policy_set, show_menu_each_step)
-
-    #Show a barplot of each player and policy, if
-    # you play more than one game and have the
-    # menu turned off.
-    if not show_menu_each_step and n>1:
-        plot_bar_plot(win_results, plot_filename, do_save_figure)
+        win_counts = dict()
+        for i in win_results:
+            win_counts[i] = win_counts.get(i, 0) + 1
+        
+        if win_counts[sorted(list(win_counts.keys()))[0]] != 0:
+            agent_a_win_count_list.append(win_counts[sorted(list(win_counts.keys()))[0]])
+        else:
+            agent_a_win_count_list.append(0)
+        if win_counts[sorted(list(win_counts.keys()))[1]] != 0:
+            agent_b_win_count_list.append(win_counts[sorted(list(win_counts.keys()))[1]])
+        else:
+            agent_b_win_count_list.append(0)
+        if win_counts[sorted(list(win_counts.keys()))[2]] != 0:
+            agent_c_win_count_list.append(win_counts[sorted(list(win_counts.keys()))[2]])
+        else:
+            agent_c_win_count_list.append(0)
+    
+    #plot the final error bar plot for the given policies and agents
+    plot_error_bar(agent_a_win_count_list, agent_b_win_count_list, agent_c_win_count_list, win_counts, do_save_figure, do_show_figure, plot_filename)
 
 
+########################################################
+#the amount of games that will be played
+n = 50
 
-run_avg_experiment(1)
+#average amount. There will be n number of games m times
+# This to ensure we can plot a barplot with standard deviation.
+m = 4
+## AMOUNT OF GAMES PLAYED IN TOTAL : m*n 
+
+
+#Whether you want to save the output barplot to a 
+# file or not. It might overwrite an existing
+# plot if you run the exact same settings as a
+# previous experiment. It will be located in the
+# folder "plots/"
+do_save_figure = True
+
+#whether you want to a see a barplot of an individual game
+do_show_figure = True
+
+
+
+#Set to true if you want a menu after each step
+# in the game. If set to False, you can run
+# many games and see results of them in a
+# barplot.
+show_menu_each_step = False
+
+#all available policies
+#Choose a policy for each agent:
+    # RANDOM                  = 0     #Chooses a random possible move
+    # CHOOSE_OTHER_PLAYER     = 1     #Favors choosing an announcement about another players rather than himself
+    # CHOOSE_THEMSELVES 	  = 2     #Favors choosing an announcement about himself rather than about another player.
+    # ARGMIN                  = 3     #Chooses the announcement that results in the lowest amount of possible worlds remaining after the announcement is made.
+    # ARGMAX                  = 4     #Chooses the announcement that results in the highest amount of possible worlds remaining afther the announcement is made.
+all_policies = [Policies.RANDOM, Policies.ARGMAX, Policies.ARGMIN, Policies.CHOOSE_OTHER_PLAYER, Policies.CHOOSE_THEMSELVES]
+
+#loop over all policies: 
+for pol1 in all_policies:
+    for pol2 in all_policies:
+        for pol3 in all_policies:
+            policy_set = [pol1, pol2, pol3]
+            #Create a plot filename based on program input 
+            # parameters (policies and amount of games played).
+            plot_filename = create_plot_filename(policy_set, n)
+            
+            run_avg_experiment(m, n, policy_set,do_save_figure, do_show_figure, plot_filename, show_menu_each_step)
